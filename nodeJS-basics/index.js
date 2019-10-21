66512b151867970267a95975521ae8824d225ea3
Siarhei <acharne@tut.by>
2019-10-22 00:39:47 +0300

    task11. Create easy REST-server

--- nodeJS-basics/index.js
+++ nodeJS-basics/index.js
@@ -24,6 +24,12 @@ const server = http.createServer((req, res) => {
 
 				res.end(content)
 			})
+		} else if (req.url === '/api/users') {
+			res.writeHead(200, { 'Content-Type': 'text/json' })
+
+			const users = [{ name: 'Max', age: 25 }, { name: 'Elena', age: 23 }]
+
+			res.end(JSON.stringify(users))
 		}
 	} else if (req.method === 'POST') {
 		const body = []
