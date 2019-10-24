a532795544f21328b3954d6a740de542b696508c
Siarhei <acharne@tut.by>
2019-10-25 00:35:03 +0300

    task08. Form processing

--- node-express/index.js
+++ node-express/index.js
@@ -17,6 +17,8 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/', homeRouts)
 app.use('/add', addRouts)
 app.use('/courses', courseRouts)
