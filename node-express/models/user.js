b9a7c808fcfd1af5f2cc581c33a2947a389b961d
Siarhei <acharne@tut.by>
2019-11-17 02:04:06 +0300

    task8. Add product to cart

--- node-express/models/user.js
+++ node-express/models/user.js
@@ -3,11 +3,11 @@ const { Schema, model } = require('mongoose')
 const userSchema = new Schema({
 	email: {
 		type: String,
-		required: true
+		required: true,
 	},
 	name: {
 		type: String,
-		required: true
+		required: true,
 	},
 	cart: {
 		items: [
@@ -15,16 +15,38 @@ const userSchema = new Schema({
 				count: {
 					type: Number,
 					required: true,
-					default: 1
+					default: 1,
 				},
 				courseId: {
 					type: Schema.Types.ObjectId,
 					ref: 'Course',
-					required: true
-				}
-			}
-		]
-	}
+					required: true,
+				},
+			},
+		],
+	},
 })
+
+userSchema.methods.addToCart = function(course) {
+	const items = [...this.cart.items]
+	const idx = items.findIndex(c => {
+		return c.courseId.toString() === course._id.toString()
+	})
+
+	if (idx >= 0) {
+		items[idx].count = items[idx].count + 1
+	} else {
+		items.push({
+			courseId: course._id,
+			count: 1
+		})
+	}
+
+	// const newCart = {items: clonedItems}
+	// this.cart = newCart
+
+	this.cart={items}
+	return this.save()
+}
 
 module.exports = model('User', userSchema)
